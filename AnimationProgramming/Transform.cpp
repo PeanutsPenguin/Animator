faf2919eda1c99a847a486c9c7cc74b0e0ad786c
Malo-aka-PeanutsPenguin <m.sadoine@student.isartdigital.com>
2025-01-13 11:28:56 -0500

    Create the Transform struct

--- AnimationProgramming/Transform.cpp
+++ AnimationProgramming/Transform.cpp
@@ -1,1 +1,36 @@
 #include "Transform.h"
+
+Transform::operator LibMath::Mat4() const
+{
+	LibMath::Mat4 result = LibMath::Mat4::Identity();
+
+	result *= LibMath::Mat4::Translate(this->mPosition);
+	result *= LibMath::Quat::Rotation(this->mRotation);
+
+	return result;
+}
+
+Transform operator*(Transform pLeft, Transform const& pRight)
+{
+	pLeft.mPosition = LibMath::Quat::Rotate(pRight.mRotation, { pLeft.mPosition.x, pLeft.mPosition.y, pLeft.mPosition.z , 0 });
+
+	pLeft.mPosition += pRight.mPosition;
+	pLeft.mRotation = pRight.mRotation * pLeft.mRotation;
+
+	return pLeft;
+}
+
+Transform& operator*=(Transform& pLeftRef, Transform const& pRight)
+{
+	pLeftRef.mPosition = LibMath::Quat::Rotate(pRight.mRotation, {  pLeftRef.mPosition.x, pLeftRef.mPosition.y, pLeftRef.mPosition.z , 0});
+
+	pLeftRef.mPosition += pRight.mPosition;
+	pLeftRef.mRotation = pRight.mRotation * pLeftRef.mRotation;
+
+	return pLeftRef;
+}
+
+Transform interpolate(Transform const& pLeft, Transform const& pRight, float pAlpha)
+{
+	return Transform();
+}
--- AnimationProgramming/Transform.h
+++ AnimationProgramming/Transform.h
@@ -1,2 +1,21 @@
 #pragma once
 
+#include "LibMath/Vector/Vec3.h"
+#include "LibMath/quat.h"
+#include "LibMath/Matrix/Mat4x4.h"
+
+struct Transform
+{
+	LibMath::Vector3 mPosition;
+	LibMath::Quat mRotation;
+
+	operator LibMath::Mat4() const;
+};
+
+Transform operator-(Transform const& pRight);
+
+Transform operator*(Transform pLeft, Transform const& pRight);
+
+Transform& operator*=(Transform& pLeftRef, Transform const& pRight);
+
+Transform interpolate(Transform const& pLeft, Transform const& pRight, float pAlpha);
